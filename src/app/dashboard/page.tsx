f0b92b790d8955f60d1fb62d9370fc5a14656f5d
CEN Smart <echinnannachukwuemeka@gmail.com>
2023-08-17 19:52:26 +0100

    dashboard page uploaded

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,7 @@
-'use client'
+
 import Container from '@/components/Container'
 import CustomButton from '@/components/CustomButton'
 import SecondaryHeading from '@/components/SecondaryHeading'
-import { Flex, Stack } from '@chakra-ui/react'
 import { FaExchangeAlt } from 'react-icons/fa'
 import { BsCurrencyExchange } from 'react-icons/bs'
 import DashboardCard from '@/components/DashboardCard'
@@ -10,41 +9,29 @@ import { HiOutlineCurrencyDollar } from 'react-icons/hi'
 import { TbUsersGroup } from 'react-icons/tb'
 import CardAvatar from '@/components/CardAvatar'
 
-
+export const metadata:Metadate = {
+  title: 'Crowda | dashboard',
+  description: 'User dashboard'
+}
 
 export default function Dashboard() {
   return (
     <>
       <Container className='pt-28' >
-        <Stack spacing={8} >
-          <Flex gap={4} flexDir={{
-            base: 'column',
-            lg: 'row'
-          }} alignItems={{
-            base: 'flex-start',
-            lg: 'center'
-          }} justifyContent={{
-            base: 'flex-start',
-            lg: 'space-between'
-          }} >
+        <div className='space-y-10' >
+          <div className='flex flex-col lg:flex-row gap-6 items-start lg:items-center justify-start lg:justify-between' >
             <SecondaryHeading heading='Hello Poppins,' title='You can manage your listed projects and do much more here.' />
-            <Flex flexDir={{
-              base: 'column',
-              md: 'row'
-            }} gap={6}>
+            <div className='flex flex-col md:flex-row gap-8'>
               <CustomButton title='Change Ownership' textColor='black' border icon={FaExchangeAlt} />
               <CustomButton title='Change Buy-in' textColor='black' border icon={BsCurrencyExchange} />
-            </Flex>
-          </Flex>
-          <Flex flexDir={{
-            base: 'column',
-            md: 'row'
-          }} gap={5}>
+            </div>
+          </div>
+          <div className='flex flex-col md:flex-row gap-8'>
             <DashboardCard bgColor='limegreen.400' icon={HiOutlineCurrencyDollar} iconLabel='Current balance' currencyValue='$5,000.66' footerText='Available to withdraw' />
             <DashboardCard bgColor='gray.200' icon={TbUsersGroup} iconLabel='Total backers' currencyValue='10,000' link='view all' />
-          </Flex>
+          </div>
           <CardAvatar />
-        </Stack>
+        </div>
       </Container>
     </>
   )
