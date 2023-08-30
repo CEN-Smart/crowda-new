7ba30883e2f442e31e88f2897958016d60682449
Ebuka <ebuka2264@yahoo.com>
2023-08-30 11:43:54 +0100

    server:deployed

--- src/server.js
+++ src/server.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const server = axios.create({
- // baseURL: "http://localhost:8000",
-  baseURL: 'https://malaika-backend-server.onrender.com/'
+  baseURL: "http://localhost:8000",
   
 });
 
