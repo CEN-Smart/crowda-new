3cb9b1f52cc8e5242f9d250ecc12f98a7baa30cb
Ebuka <ebuka2264@yahoo.com>
2023-08-30 11:01:52 +0100

    baseUrl

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const server = axios.create({
-  baseURL: "http://localhost:8000",
+ // baseURL: "http://localhost:8000",
+  baseURL: 'https://malaika-backend-server.onrender.com/'
+  
 });
 
 export default server;
